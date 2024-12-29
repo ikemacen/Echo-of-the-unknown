2fbd93c4272e1d2c0cc872705513e06b56a41aa8
ylu149 <112187763+ylu149@users.noreply.github.com>
2024-12-29 00:58:36 -0500

    More camera changes
    Smoothed out movement and changed the character to move in the direction of the mouse.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 5f, rotationSpeed = 500f;
     CameraController cameraController;
-
+    Quaternion targetRotation;
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
     }
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal"), v = Input.GetAxis("Vertical");
+        float moveAmount = Mathf.Abs(h) + Mathf.Abs(v);
 
         var moveInput = (new Vector3(h, 0, v)).normalized;
         var moveDir = cameraController.PlanarRotation * moveInput;
 
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        if(moveAmount > 0)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            targetRotation = Quaternion.LookRotation(moveDir);
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 
+            rotationSpeed * Time.deltaTime);
     }
 }
