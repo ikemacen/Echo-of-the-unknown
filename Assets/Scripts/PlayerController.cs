3375a7d8eb4e700421917875a1082ced705efb3c
ylu149 <112187763+ylu149@users.noreply.github.com>
2024-12-31 02:42:48 -0500

    Fully added temporary demo animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f, rotationSpeed = 500f;
     CameraController cameraController;
+    Animator animator;
     Quaternion targetRotation;
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
+        animator = GetComponent<Animator>();
+
     }
     private void Update()
     {
         float h = Input.GetAxis("Horizontal"), v = Input.GetAxis("Vertical");
-        float moveAmount = Mathf.Abs(h) + Mathf.Abs(v);
+        float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
 
         var moveInput = (new Vector3(h, 0, v)).normalized;
         var moveDir = cameraController.PlanarRotation * moveInput;
@@ -25,5 +28,6 @@ public class PlayerController : MonoBehaviour
         }
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 
             rotationSpeed * Time.deltaTime);
+        animator.SetFloat("Blend", moveAmount);
     }
 }
